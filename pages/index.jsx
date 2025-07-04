020bd879e8700bc9a821d3b78d76327fd8699d20
mikiimikiii <mikayiltasdemir@gmail.com>
2025-07-05 00:05:11 +0200

    Add files via upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,26 @@
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-
-const AudioVisualizer = dynamic(() => import('../visualizer'), { ssr: false });
+import Head from 'next/head';
+import Visualizer from '../components/Visualizer';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Audio Visualizer</title>
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Glitchcore Audio Visualizer</title>
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+        {/* Analytics placeholder */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXX-X"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-XXXXX-X');
+          `,
+        }} />
       </Head>
-      <AudioVisualizer />
+      <Visualizer />
     </>
   );
 }
